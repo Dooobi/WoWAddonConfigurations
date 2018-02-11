8749b5d08da5463148dfcd9ce9a584e32ad0a50f
Demindh <tobias.stelzer93@gmx.de>
2018-02-11 14:59:59 +0100

    Initial commit

---
+++ SavedVariables/Blizzard_BattlefieldMinimap.lua
@@ -0,0 +1,10 @@
+
+BattlefieldMinimapOptions = {
+	["showPlayers"] = true,
+	["locked"] = true,
+	["opacity"] = 0.7,
+	["position"] = {
+		["y"] = 350.5422838643523,
+		["x"] = 1235.944787151898,
+	},
+}
---
+++ SavedVariables/TheoryCraft.lua
@@ -0,0 +1,207 @@
+
+TheoryCraft_Settings = {
+	["FontSize"] = 11,
+	["dpm"] = true,
+	["resistscores"] = {
+		["Frost"] = 0,
+		["Arcane"] = 0,
+		["Fire"] = 0,
+		["Shadow"] = 0,
+		["Nature"] = 0,
+	},
+	["ColR"] = 1,
+	["ColG"] = 1,
+	["ColR2"] = 1,
+	["damcoef"] = true,
+	["trysecondsfg"] = 2,
+	["dotoverct"] = true,
+	["FontPath"] = "Fonts\\ArialN.TTF",
+	["dontresist"] = true,
+	["CustomOutfit"] = {
+		["destat"] = {
+			[1] = "Head",
+			[2] = "Neck",
+			[3] = "Shoulder",
+			[4] = "Chest",
+			[5] = "Waist",
+			[6] = "Legs",
+			[7] = "Feet",
+			[8] = "Wrist",
+			[9] = "Hands",
+			[10] = "Finger0",
+			[11] = "Finger1",
+			[12] = "Trinket0",
+			[13] = "Trinket1",
+			[14] = "Back",
+			[15] = "MainHand",
+			[16] = "SecondaryHand",
+			[17] = "Ranged",
+			[18] = "Tabard",
+			[19] = "Ammo",
+		},
+		["newstat"] = {
+		},
+		["strength"] = 0,
+		["agility"] = 0,
+		["name"] = "Naked",
+		["slots"] = {
+		},
+		["wear"] = {
+		},
+		["intellect"] = 0,
+		["spirit"] = 0,
+		["stamina"] = 0,
+	},
+	["critchancedata"] = {
+		["239:6"] = {
+			["casts"] = 210,
+			["crits"] = 34,
+		},
+		["320:8"] = {
+			["casts"] = 251,
+			["crits"] = 44,
+		},
+		["270:6"] = {
+			["casts"] = 290,
+			["crits"] = 41,
+		},
+		["289:8"] = {
+			["casts"] = 88,
+			["crits"] = 16,
+		},
+		["296:6"] = {
+			["casts"] = 46,
+			["crits"] = 4,
+		},
+		["294:8"] = {
+			["casts"] = 75,
+			["crits"] = 5,
+		},
+		["227:6"] = {
+			["casts"] = 38,
+			["crits"] = 5,
+		},
+		["307:6"] = {
+			["casts"] = 2,
+			["crits"] = 0,
+		},
+		["331:8"] = {
+			["casts"] = 404,
+			["crits"] = 59,
+		},
+		["302:7"] = {
+			["casts"] = 25,
+			["crits"] = 3,
+		},
+		["303:6"] = {
+			["casts"] = 131,
+			["crits"] = 20,
+		},
+		["330:8"] = {
+			["casts"] = 76,
+			["crits"] = 14,
+		},
+		["318:7"] = {
+			["casts"] = 98,
+			["crits"] = 11,
+		},
+		["280:6"] = {
+			["casts"] = 115,
+			["crits"] = 19,
+		},
+		["249:6"] = {
+			["casts"] = 4,
+			["crits"] = 0,
+		},
+		["228:5"] = {
+			["casts"] = 39,
+			["crits"] = 1,
+		},
+		["329:7"] = {
+			["casts"] = 190,
+			["crits"] = 22,
+		},
+		["140:6"] = {
+			["casts"] = 3,
+			["crits"] = 0,
+		},
+		["258:6"] = {
+			["casts"] = 198,
+			["crits"] = 32,
+		},
+		["299:8"] = {
+			["casts"] = 110,
+			["crits"] = 22,
+		},
+		["282:7"] = {
+			["casts"] = 236,
+			["crits"] = 37,
+		},
+		["310:6"] = {
+			["casts"] = 108,
+			["crits"] = 17,
+		},
+		["263:6"] = {
+			["casts"] = 4,
+			["crits"] = 0,
+		},
+		["313:7"] = {
+			["casts"] = 253,
+			["crits"] = 34,
+		},
+		["119:6"] = {
+			["casts"] = 3,
+			["crits"] = 0,
+		},
+		["317:7"] = {
+			["casts"] = 4,
+			["crits"] = 1,
+		},
+		["274:6"] = {
+			["casts"] = 37,
+			["crits"] = 5,
+		},
+		["294:6"] = {
+			["casts"] = 32,
+			["crits"] = 2,
+		},
+		["300:8"] = {
+			["casts"] = 44,
+			["crits"] = 8,
+		},
+		["282:10"] = {
+			["casts"] = 1,
+			["crits"] = 0,
+		},
+		["299:6"] = {
+			["casts"] = 147,
+			["crits"] = 15,
+		},
+	},
+	["combinedot"] = true,
+	["mana"] = true,
+	["ColG2"] = 1,
+	["hpm"] = true,
+	["max"] = true,
+	["ColB"] = 1,
+	["buttontextx"] = 1.11100267934368,
+	["tryfirst"] = "averagedam",
+	["dataversion"] = "1.06",
+	["dameff"] = true,
+	["tryfirstsfg"] = 0,
+	["hps"] = true,
+	["buttontexty"] = 10.21999915063047,
+	["ColB2"] = 0.6862745098039216,
+	["buttontext"] = true,
+	["dps"] = true,
+	["trysecond"] = "hpm",
+	["titles"] = true,
+	["plusdam"] = true,
+	["dpsdam"] = true,
+	["GenerateList"] = "",
+	["averagedam"] = true,
+	["damfinal"] = true,
+	["crit"] = true,
+	["embed"] = true,
+	["healanddamage"] = true,
+}
